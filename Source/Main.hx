66aaa17a6290d58aab114c0c0eba79ae6ee1ff98
saumya <2saumyaray@gmail.com>
2016-07-02 18:32:14 +0530

    Fix: Neko is not working for this!

--- Source/Main.hx
+++ Source/Main.hx
@@ -2,6 +2,7 @@ package;
 
 
 import openfl.display.Sprite;
+import openfl.events.Event;
 
 import com.saumya.GameEntry;
 
@@ -12,8 +13,14 @@ class Main extends Sprite {
 	public function new () {
 		
 		super ();
+		this.addEventListener(Event.ADDED_TO_STAGE,onAddedToStage);
 		
 		
+	}
+
+	private function onAddedToStage(e:Event):Void{
+		this.removeEventListener(Event.ADDED_TO_STAGE,onAddedToStage);
+
 		var game:GameEntry = new GameEntry();
 		addChild(game);
 	}
--- Source/com/saumya/GameEntry.hx
+++ Source/com/saumya/GameEntry.hx
@@ -1,23 +1,45 @@
 package com.saumya;
 
+import openfl.display.Graphics;
 import openfl.display.Sprite;
 
 import ru.stablex.ui.UIBuilder;
 
+import ru.stablex.ui.widgets.Text;
+
 class GameEntry extends Sprite {
 	
+	private var t:Text;
+
 	public function new() {
 		super();
+
+		initialise();
 	}
 
 	public function initialise():Void{
 		trace("GameEntry : initialise : ");
 
 		UIBuilder.init();
+		construct();
 	}
 
 	public function construct():Void{
 		trace("GameEntry : construct : ");
+
+		var g:Graphics = this.graphics;
+		g.clear();
+		g.beginFill(0xFF0000);
+		g.drawRect(0,0,200,400);
+		g.endFill();
+		
+		this.t = UIBuilder.create(Text, {
+			w: 200, h: 40,
+		    left : 0, top  : 0,
+		    text : 'My first widget!'
+		});
+		addChild(this.t);
+		
 	}
 
 	public function restart(?newWidth:Float,?newHeight:Float):Void{
